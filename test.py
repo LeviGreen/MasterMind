bb90ff12c53dca5e7fd954035e32d4e35b5fdb44
LeviGreen <lgreen.design@comcast.net>
2021-03-12 06:09:01 -0500

    push/pull test on test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+from termcolor import colored
+
+print(colored('0', 'red'))
+
+print(colored('0', 'yellow'))
+
+print(colored('0', 'green'))
+
+print(colored('0', 'blue'))
+
+print(colored('0', 'white'))
+
+print(colored('0', 'magenta'))
