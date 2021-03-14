29dbc5a712b8f4a63ab5154211fe103f1e7f196c
LeviGreen <lgreen.design@comcast.net>
2021-03-14 06:13:43 -0500

    refactored while's into for's where I could

--- MM_ManualChange.py
+++ MM_ManualChange.py
@@ -47,41 +47,36 @@ def Initial():
 #Function that generates a random pattern.
 def GeneratePattern():
     compArr = [] #declare empty array
-    i = 0
-    while i < 4: 
+    for x in range(4):
         compPick = random.randint(1,6) #Generate random int between 1 and 6.
         compArr.append(compPick)
-        i = i + 1
     return compArr #Return pattern.
 
 #Use for debugging. Designate the computer pattern.
 def KnownPattern():
     return [2,2,2,2]
+
 #Function to compare user input with computer pattern
 def ComparePatterns(c0,c1,c2,c3,u0,u1,u2,u3):
     comp = [c0,c1,c2,c3] #Array to hold computer pattern
     user = [u0,u1,u2,u3]
     resultArr = [] #Declare empty array for outputted data.
 
-    c = 0
-    while c < 4: #This while loop checks for right color and right spot.
+    for c in range(4): #This for loop checks for right color and right spot.
         if int(user[c]) == comp[c]:  #if right color and spot...
             resultArr.insert(0,2)   #Add 2 to results array
             user[c] = 0             #clear user and comp array to 
             comp[c] = 0             #handle duplicate numbers. 
-        c = c + 1
 
-    c = 0
-    while c < 4: #This while loop checks for right color and wrong spot.
+    for c in range(4): #This for loop checks for right color and wrong spot.
         if int(user[c]) > 0 and int(user[c]) in comp: #if user color is in comp pattern
             comp[comp.index(int(user[c]))] = 0 #clear comp array to handle duplicate numbers.
-            resultArr.insert(0,1)       #Add 1 to results array.
-        c = c + 1              
+            resultArr.insert(0,1)       #Add 1 to results array.             
 
     runningScore.append(resultArr)
     return sum(resultArr)           #return sum of the resultsArr (8 = win)
 
-
+#Prints the game as it stands to the terminal
 def ShowRunningGame():
     for x in range(50):
         print
@@ -103,24 +98,20 @@ def EndGame(result,pattern):
 
 #######################Code Start#########################################################################
     
-tries = 1
+tries = 0
 result = 0
 
 Initial()
 pattern = GeneratePattern() #Generate Random Pattern
 #pattern = KnownPattern()
-
 print("time to guess four colors:")
 
-while tries <= 10 and result < 8: #gives user 10 tries to get right answer.
-    userPick = MM_UserPickPattern.UserPickPattern()
-    runningGuess.append(userPick)
+while tries < 10 and result < 8: #gives user 10 tries to get right answer.
+    userPick = MM_UserPickPattern.UserPickPattern() #prompt user to pick another pattern
+    runningGuess.append(userPick) #update r.g. array
     result = ComparePatterns(pattern[0],pattern[1],pattern[2],pattern[3], userPick[0],userPick[1],userPick[2],userPick[3])
-    
     ShowRunningGame()
-    
-    tries = tries + 1 #increment try count.        
-
+    tries = tries + 1 #increment try count.
 
 EndGame(result,pattern)
 
